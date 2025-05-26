c5d1a303bf1aca93ae842c47fd91fe152b6d841a
Baljan Kanatbek <baljann19@gmail.com>
2025-05-26 23:19:09 +0300

    Initialize Firebase React App with basic structure and authentication component
    - Created package.json with dependencies for React, Firebase, and testing libraries.
    - Added favicon and logo images to the public directory.
    - Created index.html as the main HTML template for the app.
    - Added manifest.json for PWA support.
    - Included robots.txt for search engine indexing.
    - Created App.css for styling the application.
    - Developed App.js as the main component, integrating the Auth component.
    - Implemented Auth component for user authentication with email and password inputs.
    - Configured Firebase in firebase.js for authentication.
    - Set up index.js to render the App component.

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoZRDix6bNqsD_Rl1NxS4EfynZLCCtChs",
+  authDomain: "fir-react-app-79884.firebaseapp.com",
+  projectId: "fir-react-app-79884",
+  storageBucket: "fir-react-app-79884.firebasestorage.app",
+  messagingSenderId: "507388813007",
+  appId: "1:507388813007:web:47c27e216f382faf95e1ba",
+  measurementId: "G-3DWM3KDBDT",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
