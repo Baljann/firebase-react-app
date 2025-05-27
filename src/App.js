722493a2b4026bb57b171d625d2622b6a1b9c723
Baljan Kanatbek <baljann19@gmail.com>
2025-05-27 18:02:06 +0300

    Implement movie data fetching and enhance authentication with Google sign-in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Auth } from "./components/auth";
+import { dataBase } from "./config/firebase";
+import { getDocs, collection } from "firebase/firestore";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  const moviesCollectionRef = collection(dataBase, "movies");
+
+  useEffect(() => {
+    const getMovieList = async () => {
+      //READ THE DATA
+      try {
+        const data = await getDocs(moviesCollectionRef);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setMovieList(filteredData);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getMovieList();
+  }, []);
+
+  //SET THE DATA
   return (
     <div className="App">
       <Auth />
+      <div>
+        {movieList.map((movie) => (
+          <div>
+            <h1 style={{ color: movie.receivedAnOscar ? "green" : "red" }}>
+              {movie.title}
+            </h1>
+            <p>Date: {movie.releaseDate}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAoZRDix6bNqsD_Rl1NxS4EfynZLCCtChs",
@@ -12,4 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const dataBase = getFirestore(app)
